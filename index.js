bd1231b41bc05dccd5c7c492a4dd6bb9a2185fce
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-27 10:05:26 +0100

    made edits and added rroute for delet

--- index.js
+++ index.js
@@ -6,37 +6,36 @@ const bodyParser = require('body-parser')
 //init express app
 const app = express()
 
-//article model
-
-const Article = require("./models/articles")
-
 //connect to mongo db
 mongoose.connect("mongodb://localhost/Blog")
 const db = mongoose.connection
 
 //check connection
-
 db.once('open',()=>{
     console.log("Connected to mongo DB")
 })
-
+//if error coonection error
 db.on("error" , (err)=>{
     console.log( err)
 })
 
-
 //init bodyparser
-
 app.use( bodyParser.urlencoded({extended:false}))
 
 //parse data to json
 app.use(bodyParser.json())
 
-
+//the public page 
 app.use(express.static(path.join(__dirname,"public")))
 
-//route for index page
+//views 
+app.set("views", path.join(__dirname,"views"))
+app.set("view engine", "pug")
 
+const Article = require("./models/articles")
+
+
+//route for index page
 Article.find({},(err, articles)=>{
     if (err) {
         console.log(err)
@@ -50,6 +49,7 @@ Article.find({},(err, articles)=>{
       
     }
 })
+
 // add article path
 app.get('/add/articles' , (req, res)=>{
     res.render('add_article', {
@@ -57,6 +57,8 @@ app.get('/add/articles' , (req, res)=>{
     })
 })
 
+
+//adding an article to the database
 app.post("/add/articles",(req,res)=>{
     const article = new Article()
     article.title= req.body.title
@@ -73,10 +75,54 @@ app.post("/add/articles",(req,res)=>{
     })
 })
 
-//views 
-app.set("views", path.join(__dirname,"views"))
-app.set("view engine", "pug")
 
+//get article by id
+app.get("/:id", (req, res)=>{
+      Article.findById(req.params.id, (err, article)=>{
+          if (err) {
+              console.log(err)
+          } else {
+            res.render("article",{
+                article:article   
+            })
+          }
+      } )
+
+})
+
+//edit article and save
+app.post("/articles/edit/:id",(req,res)=>{
+    const article = {}
+    article.title= req.body.title
+    article.author= req.body.author
+    article.body = req.body.body
+
+    const query = {_id:req.params.id}
+    console.log(query)
+    Article.update(query,article,(err)=>{
+        if(err){
+         console.log(err)
+         console.log('error ooo')
+         return;
+        }
+        else{
+        res.redirect("/")
+        }
+    })
+})
+
+//send get request for edit
+app.get("/articles/edit/:id",(req,res)=>{
+    Article.findById(req.params.id, (err, article)=>{
+        if (err) {
+            console.log(err)
+        } else {
+          res.render("edit_article",{
+              article:article   
+          })
+        }
+    } )
+})
 
 app.listen(5000, ()=>{
     console.log("App started on server 5000")
